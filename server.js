5197a3839ac48fd2e14e820be25181309360cdfc
Aysha Hira <106012619+AestheticChallenger@users.noreply.github.com>
2025-07-27 20:57:12 +0400

    Redisigned the websites, removed not needed html pages

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-// const express = require('express');
-// const app = express();
-
-// const path = require('path');
-
 const express = require('express');
+const path = require('path');
 const app = express();
 
-const path = require('path');
+// Serve static folders with proper capitalization
+app.use('/IMAGES', express.static(path.join(__dirname, 'IMAGES')));
+app.use('/CSS', express.static(path.join(__dirname, 'CSS')));
+app.use('/JAVASCRIPT', express.static(path.join(__dirname, 'JAVASCRIPT')));
 
+// serve HTML_PAGES static for gettingt direct URL access
+app.use(express.static(path.join(__dirname, 'HTML_PAGES')));
+
+// Routes for HTML pages
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'HTML_PAGES', 'Home.html'));
 });
@@ -17,8 +20,8 @@ app.get('/adopt', (req, res) => {
     res.sendFile(path.join(__dirname, 'HTML_PAGES', 'AdoptAPet.html'));
 });
 
-app.get('/about-us', (req, res) => {
-    res.sendFile(path.join(__dirname, 'HTML_PAGES', 'AboutUs.html'));
+app.get('/about-developers', (req, res) => {
+    res.sendFile(path.join(__dirname, 'HTML_PAGES', 'AboutDevelopers.html'));
 });
 
 app.get('/pet-products', (req, res) => {
@@ -26,8 +29,11 @@ app.get('/pet-products', (req, res) => {
 });
 
 // we can have one page just for cart
+app.get('/cart', (req, res) => {
+    res.sendFile(path.join(__dirname, 'HTML_PAGES', 'Payment.html'));
+});
 
 
 app.listen(7942, () => {
-    console.log('Server is running on port 7942');
-})
+    console.log('🚀 Server is running at http://localhost:7942');
+});
